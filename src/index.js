02c9936b33b48c1c84e76592be9c8ac2b56444e4
Mooshi <rchiriac16@gmail.com>
2022-11-14 20:05:15 +0000

    progress update

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const btn_login = document.getElementById("btn_login");
+
+btn_login.addEventListener("click", () => {
+  alert("Success: The Login button was pressed!");
+  console.log("Login button pressed");
+});
+
+const btn_signup = document.getElementById("btn_signup");
+
+btn_signup.addEventListener("click", () => {
+  alert("Success: The Signup button was pressed!");
+  console.log("Signup button pressed");
+});
