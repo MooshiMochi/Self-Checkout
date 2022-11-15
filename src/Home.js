1b115f146794f065b9808a1601c92a52316f7ff4
Mooshi <rchiriac16@gmail.com>
2022-11-15 23:19:53 +0000

    changed font fam

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
     <>
       <Helmet>
-        <meta charset='UTF-8' />
-        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <meta property='og:image' content='/logo.svg' />
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:image" content="/logo.svg" />
         <title>Age Verification | Home</title>
       </Helmet>
       <main>
-        <h1 className=''>Age Verification</h1>
-        <div className='main-div'>
-          <div className='flex space-x-5'>
-            <button id='btn_login' className='w-24'>
+        <h1 className="font-mono">Age Verification</h1>
+        <div className="main-div">
+          <div className="flex space-x-5 justify-center">
+            <button id="btn_login" className="w-24 m-auto">
               Login
             </button>
-            <button id='btn_signup' className='w-24'>
+            <button id="btn_signup" className="w-24 m-auto">
               Sign up
             </button>
           </div>
           <div>
-            <a href='#/'>
+            <a href="#/">
               <i>Forgot password</i>
             </a>
           </div>
