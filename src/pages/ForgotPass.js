633f162365b30d9aab0b61d1c49ab194932db035
Mooshi <rchiriac16@gmail.com>
2022-11-19 21:58:48 +0000

    bullshit incoming

--- src/pages/ForgotPass.js
+++ src/pages/ForgotPass.js
@@ -1,20 +1,31 @@
 import { Button, TextInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
 import { React, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { useNavigate } from 'react-router-dom';
 
 export default function ForgotPass() {
   const navigate = useNavigate();
-  const [enterCode, showEnterCode] = useState(false);
+  // let form = useForm({
+  // const [enterCode, showEnterCode] = useState(false);
+
+  const form = useForm({
+    initialValues: {
+      email: '',
+    },
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+    },
+    validateInputOnChange: true,
+  });
+
   const [passwordReset, setPasswordReset] = useState(false);
   const [clicked, setClicked] = useState(0);
   let page = !passwordReset ? (
     <div>
-      <h1 className='text-6xl flex flex-col justify-center'>
-        Forgot Password
-      </h1>
+      <h1 className='text-6xl flex flex-col justify-center'>Forgot Password</h1>
       <h2 className='text-base align-text-top flex flex-col justify-center'>
-        {enterCode ? (
+        {clicked === 1 ? (
           <div>
             We've found the email ... linked to your account<br></br>
             Please enter the verification code sent
@@ -24,13 +35,21 @@ export default function ForgotPass() {
         )}
       </h2>
 
-      <form className='flex flex-col'>
+      <form
+        onSubmit={form.onSubmit((values) => {
+          console.log(values);
+          form.setFieldValue('email', '');
+        })}
+        className='flex flex-col'
+        action='/TEST_TARGET'
+      >
         <TextInput
           type='email'
           name='email'
           id='email'
-          placeholder='Enter your email'
+          placeholder='your@email.com'
           className='lg:w-1/3 mt-4'
+          {...form.getInputProps('email')}
         />
         <div className='lg:w-1/6 lg:h-6'>
           <Button
@@ -39,19 +58,9 @@ export default function ForgotPass() {
             color='green'
             className='lg:w-1/2 lg:h-1/12 mt-2 rounded-lg border-4 border-teal-300 shadow-inner shadow-gray-600 text-inherit'
             onClick={() => {
-              showEnterCode(true);
               setClicked(clicked + 1);
-              clicked == 2 ? setPasswordReset(true) : setPasswordReset(false);
-              document.getElementById('email').value = '';
-              // clear the input field
-
-              // send email to user
-
-              // show the input field for the code
-
-              // show the button to submit the code
-
-              // show the button to resend the code
+              clicked === 2 ? setPasswordReset(true) : setPasswordReset(false);
+              console.log('Cleared input field');
             }}
           >
             Next
@@ -61,24 +70,23 @@ export default function ForgotPass() {
     </div>
   ) : (
     <div className='text-center w-full mx-auto'>
-      <h1 className='text-6xl'>
-        Forgot Password
-      </h1>
+      <h1 className='text-6xl'>Forgot Password</h1>
       <div>Your password has been reset</div>
       <div className='lg:w-1/6 lg:h-6'>
-          <Button
-            type='submit'
-            variant='outline'
-            color='green'
-            className=' mt-2 rounded-lg border-4 border-teal-300 shadow-inner shadow-gray-600 text-inherit'
-            onClick={() => {
-              navigate('/login');
-            }}
-          >
-            Next
-          </Button>
-        </div>
-    </div>);
+        <Button
+          type='submit'
+          variant='outline'
+          color='green'
+          className=' mt-2 rounded-lg border-4 border-teal-300 shadow-inner shadow-gray-600 text-inherit'
+          onClick={() => {
+            navigate('/login');
+          }}
+        >
+          Next
+        </Button>
+      </div>
+    </div>
+  );
 
   return (
     <>
@@ -89,9 +97,7 @@ export default function ForgotPass() {
         <title>Age Verification | Forgot Password</title>
       </Helmet>
 
-      <main className='flex flex-col translate-y-1/4 m-4'>
-        {page}
-      </main>
+      <main className='flex flex-col translate-y-1/4 m-4'>{page}</main>
     </>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,7 @@
+import { Button, TextInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
+import { React } from 'react';
+import { Helmet } from 'react-helmet';
 
 export default function Login() {
   const form = useForm({
@@ -6,12 +9,85 @@ export default function Login() {
       email: '',
       password: '',
     },
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+      password: (value) =>
+        value.length > 0 ? null : 'Password must be at least 1 character long',
+    },
+    validateInputOnChange: true,
   });
 
-  const login = async (e) => {};
+  const login = async (e) => {
+    console.log('login');
+    // e.preventDefault();
+    // const res = await fetch('/api/login', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     email: form.values.email,
+    //     password: form.values.password,
+    //   }),
+    // });
+    // const data = await res.json();
+    // console.log(data);
+    // if (data.error) {
+    //   console.log(data.error);
+    // }
+    // if (data.token) {
+    //   console.log(data.token);
+    // }
+  };
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <Helmet>
+        <meta charset='UTF-8' />
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <title>Age Verification | Home</title>
+      </Helmet>
+      <div>
+        <main className='flex flex-col justify-center items-center h-screen m-auto fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-center'>
+          <h1 className='text-1x1'>Customer Login</h1>
+
+          <form
+            onSubmit={form.onSubmit((values) => {
+              console.log(values);
+            })}
+            className='flex flex-col'
+            action='/TEST_TARGET'
+          >
+            <TextInput
+              type='username'
+              name='username'
+              id='username'
+              placeholder='Username'
+              className='lg:w-1/8 mt-4'
+              {...form.getInputProps('username')}
+            />
+            <TextInput
+              type='password'
+              name='password'
+              id='password'
+              placeholder='Password'
+              className='lg:w-1/8 mt-4'
+              {...form.getInputProps('password')}
+            />
+            <Button
+              type='submit'
+              variant='outline'
+              color='green'
+              className='lg:w-1/2 lg:h-1/12 mt-2 rounded-lg border-4 border-teal-300 shadow-inner shadow-gray-600 text-inherit'
+              onClick={() => {
+                login();
+              }}
+            >
+              Sign in
+            </Button>
+          </form>
+        </main>
+      </div>
+    </>
   );
 }
