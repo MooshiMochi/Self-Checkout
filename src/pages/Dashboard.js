d5eb4a1fe6b722d3a884803e64bcb4cc28f588fb
Mooshi <rchiriac16@gmail.com>
2022-11-21 00:12:43 +0000

    started working on dash

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,8 +2,33 @@ import React from 'react';
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className='grid grid-rows-3'>
+      <div className='flex justify-between'>
+        <div>Sample QR Code</div>
+        <div className='flex flex-col'>
+          <div>Personal Details</div>
+          <div>Stuff</div>
+        </div>
+        <div>
+          <button>Log Out</button>
+        </div>
+      </div>
+      <div className='flex flex-col'>
+        <div>Home Address</div>
+        <div>Home Address Info</div>
+      </div>
+      <div>Image</div>
+      <div className='grid grid-rows-3'>
+        <button>Select an Image</button>
+        <button>Upload an Image</button>
+        {/* <input>Select an Image</input>
+        <input>Upload an image</input> */}
+      </div>
+      <div className='text-center max-w-md'>
+        If your personal details or home address are not filled then it is
+        likely that your ID is not supported or the picture is blurry. Please
+        re-upload a clearer picture or your ID.
+      </div>
     </div>
   );
 };
