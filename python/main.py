3016ccf8ddc1c41b4e5b2547e983ba1ee4dc01f3
Mooshi <rchiriac16@gmail.com>
2022-11-21 10:58:26 +0000

    started work on dashboard styling started work on python OCR

---
+++ python/main.py
@@ -0,0 +1,8 @@
+
+# Create the server using fastapi
+
+from FastApi import FastAPI
+
+app = FastAPI()
+_host = ""
+_port = ""
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,30 +1,44 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+  const base_box_style =
+    'rounded-lg shadow-lg border-4 border-teal-200 w-1/3 flex flex-col justify-center items-center bg-grey-200';
   return (
-    <div className='grid grid-rows-3'>
+    <div className='grid grid-rows-3 w-full p-5 text-center'>
       <div className='flex justify-between'>
-        <div>Sample QR Code</div>
+        <div className={base_box_style}>Sample QR Code</div>
         <div className='flex flex-col'>
           <div>Personal Details</div>
-          <div>Stuff</div>
+          <div className={base_box_style}>Stuff</div>
         </div>
         <div>
-          <button>Log Out</button>
+          <button
+            className='rounded-lg shadow-lg'
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            Log Out
+          </button>
         </div>
       </div>
-      <div className='flex flex-col'>
-        <div>Home Address</div>
-        <div>Home Address Info</div>
-      </div>
-      <div>Image</div>
-      <div className='grid grid-rows-3'>
-        <button>Select an Image</button>
-        <button>Upload an Image</button>
-        {/* <input>Select an Image</input>
+      <div className='flex justify-between border-8 border-red-800'>
+        <div className='flex flex-col'>
+          <div>Home Address</div>
+          <div className={base_box_style}>Home Address Info</div>
+        </div>
+        <div className={base_box_style}>Image</div>
+        <div className='grid grid-rows-3 text-center'>
+          <button className='rounded-lg shadow-lg'>Select an Image</button>
+          <button className='rounded-lg shadow-lg'>Upload an Image</button>
+          {/* <input>Select an Image</input>
         <input>Upload an image</input> */}
+        </div>
       </div>
-      <div className='text-center max-w-md'>
+
+      <div className='text-center max-w-md w-full translate-x-1/2 text-sm translate-y-1/4 p-0 font-bold'>
         If your personal details or home address are not filled then it is
         likely that your ID is not supported or the picture is blurry. Please
         re-upload a clearer picture or your ID.
