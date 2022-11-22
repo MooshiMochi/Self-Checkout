cc7ec9cfac955eeb9699cddae9d7b546755a7993
Mooshi <rchiriac16@gmail.com>
2022-11-22 17:40:08 +0000

    qwack

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,47 +4,105 @@ import { useNavigate } from 'react-router-dom';
 const Dashboard = () => {
   const navigate = useNavigate();
   const base_box_style =
-    'rounded-lg shadow-lg border-4 border-teal-200 w-1/3 flex flex-col justify-center items-center bg-grey-200';
+    'rounded-lg shadow-lg border-4 border-teal-300 bg-grey-300';
+  // w-1/3 flex flex-col justify-center items-center
   return (
-    <div className='grid grid-rows-3 w-full p-5 text-center'>
-      <div className='flex justify-between'>
-        <div className={base_box_style}>Sample QR Code</div>
-        <div className='flex flex-col'>
-          <div>Personal Details</div>
-          <div className={base_box_style}>Stuff</div>
+    <div className='grid grid-rows-3 w-full text-center h-full' id='TABLE'>
+      <div
+        className='flex flex-row justify-around items-center rounded-md border-red-500 border-4 relative'
+        id='ROW'
+      >
+        <div className={base_box_style + ' w-1/5'}>Sample QR Code</div>
+        <div className='w-2/5'>
+          <div className=''>Personal Details</div>
+          <div className={base_box_style + ''}>
+            Name: ...<br></br>
+            Date of Birth: ...<br></br>
+            Customer ID: ...
+          </div>
         </div>
+        <div className='w-1/5'>
+          <button onClick={() => navigate('/')}>Log Out</button>
+        </div>
+      </div>
+      <div
+        className='flex flex-row justify-around items-center rounded-md border-green-500 border-4'
+        id='ROW'
+      >
         <div>
-          <button
-            className='rounded-lg shadow-lg'
-            onClick={() => {
-              navigate('/');
-            }}
-          >
-            Log Out
-          </button>
-        </div>
-      </div>
-      <div className='flex justify-between border-8 border-red-800'>
-        <div className='flex flex-col'>
           <div>Home Address</div>
-          <div className={base_box_style}>Home Address Info</div>
+          <div className={base_box_style}>
+            House Number: ...<br></br>
+            Street Name: ...<br></br>
+            City: ...<br></br>
+            Post Code: ...
+          </div>
         </div>
-        <div className={base_box_style}>Image</div>
-        <div className='grid grid-rows-3 text-center'>
-          <button className='rounded-lg shadow-lg'>Select an Image</button>
-          <button className='rounded-lg shadow-lg'>Upload an Image</button>
-          {/* <input>Select an Image</input>
-        <input>Upload an image</input> */}
+        <div className={base_box_style}>Sample Image</div>
+        <div>
+          <div>
+            <button>Select an image</button>
+          </div>
+          <div>
+            <button>Upload an image</button>
+          </div>
         </div>
       </div>
-
-      <div className='text-center max-w-md w-full translate-x-1/2 text-sm translate-y-1/4 p-0 font-bold'>
+      <div className='rounded-md border-blue-500 border-4'>
         If your personal details or home address are not filled then it is
         likely that your ID is not supported or the picture is blurry. Please
-        re-upload a clearer picture or your ID.
+        re-upload a clearer picture of your ID.
       </div>
     </div>
   );
 };
 
 export default Dashboard;
+
+// <div className='grid grid-rows-3 w-full text-center'>
+//       <div className='flex justify-between bg-'>
+//         <div className={base_box_style}>Sample QR Code</div>
+//         <div className='flex flex-col w-1/3'>
+//           <div className='text-left'>Personal Details</div>
+//           <div className={base_box_style + ' text-left'}>
+//             Name: ...<br></br>
+//             Date of Birth: ...<br></br>
+//             Customer ID: ...
+//           </div>
+//         </div>
+//         <div>
+//           <button
+//             className={base_box_style}
+//             onClick={() => {
+//               navigate('/');
+//             }}
+//           >
+//             Log Out
+//           </button>
+//         </div>
+//       </div>
+//       <div className='flex justify-between border-8 border-red-800 w-full'>
+//         <div className='flex flex-col'>
+//           <div className='text-left'>Home Address</div>
+//           <div className={base_box_style + ' text-left'}>
+//             House Number: ...<br></br>
+//             Street Name: ...<br></br>
+//             City: ...<br></br>
+//             Post Code: ...
+//           </div>
+//         </div>
+//         <div className={base_box_style}>Image</div>
+//         <div className='grid grid-rows-3 text-center'>
+//           <button className='rounded-lg shadow-lg'>Select an Image</button>
+//           <button className='rounded-lg shadow-lg'>Upload an Image</button>
+//           {/* <input>Select an Image</input>
+//         <input>Upload an image</input> */}
+//         </div>
+//       </div>
+
+//       <div className='text-center max-w-md w-full translate-x-1/2 text-sm translate-y-1/4 p-0 font-bold'>
+//         If your personal details or home address are not filled then it is
+//         likely that your ID is not supported or the picture is blurry. Please
+//         re-upload a clearer picture or your ID.
+//       </div>
+//     </div>
